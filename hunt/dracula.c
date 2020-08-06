ab382218818bc049de8012724f568fbfa72a7a5e
sonia <sonia.sun16@gmail.com>
2020-08-06 14:17:47 +1000

    Changed placeIdToAbbrevNonConst to dracLocToMoveAbbrev

--- hunt/dracula.c
+++ hunt/dracula.c
@@ -42,7 +42,6 @@ void lowHealthMove(DraculaView dv, PlaceId furthestLoc);
 void handleRoundZero(DraculaView dv);
 int distFromHunters(DraculaView dv, PlaceId loc);
 int shortestPathFrom(DraculaView dv, Player hunter, PlaceId dest);
-char *placeIdToAbbrevNonConst(PlaceId pid);
 ClosestHunter initClosestHunter(void);
 void storeClosestHunter(DraculaView dv, ClosestHunter closestHunterInfo, 
 						PlaceId dracLoc);
@@ -67,7 +66,7 @@ void decideDraculaMove(DraculaView dv)
 
 	// Handle cases when no choice.
 	if (numLocs == 0 && round != 0) registerBestPlay("TP", "Mwahahahaha"); // TODO: CD or TP?
-	if (numLocs == 1) registerBestPlay(dracLocToMoveAbbrev(reachableLocations[0]), "...");
+	if (numLocs == 1) registerBestPlay(dracLocToMoveAbbrev(dv, reachableLocations[0]), "...");
 	// TODO: Calculate shortest means to castle dracula, if health is low
 	//if (health <= 10) {
 		// Get safest path to castle dracula
@@ -115,32 +114,32 @@ void decideDraculaMove(DraculaView dv)
 	}
 
 	// Register best play, in order of precedence.
-	// Handle case when there is no choice but to travel by sea.
-	if (furthestNonSeaLoc == CITY_UNKNOWN) registerBestPlay(dracLocToMoveAbbrev(furthestLoc), "...");
-	// Give precedence to avoiding suiciding at sea when he has 2 blood points (or less) left...
-	else if (DvGetHealth(dv, PLAYER_DRACULA) <= LIFE_LOSS_SEA) registerBestPlay(dracLocToMoveAbbrev(furthestNonSeaLoc), "...");
+	// Handle case when there is nso choice but to travel by sea.
+	if (furthestNonSeaLoc == CITY_UNKNOWN) registerBestPlay(dracLocToMoveAbbrev(dv, furthestLoc), "...");
+	// Give precedece to avoiding suiciding at sea when he has 2 blood points (or less) left...
+	else if (DvGetHealth(dv, PLAYER_DRACULA) <= LIFE_LOSS_SEA) registerBestPlay(dracLocToMoveAbbrev(dv, furthestNonSeaLoc), "...");
 	// ... then to avoiding the closest hunter, if dangerously close, health is low, and can't guarantee a hunter kill...
 	// a. This could mean heading to sea to avoid an encounter.
 	// b. This could mean maximising your distance from the hunter
 	else if (DvGetHealth(dv, PLAYER_DRACULA) <= HALF_START_HEALTH) lowHealthMove(dv, furthestLoc);
-	// else if (health <= 12 && distClosestHunter <= 1 && DvGetHealth(dv, closestHunter) > highestDamageIfEncounteredAtCity) registerBestPlay(dracLocToMoveAbbrev(furthestLoc), "...");
+	// else if (health <= 12 && distClosestHunter <= 1 && DvGetHealth(dv, closestHunter) > highestDamageIfEncounteredAtCity) registerBestPlay(dracLocToMoveAbbrev(dv, furthestLoc), "...");
 	// ... then to avoiding land if hunter is at the same sea location of Dracula, and a hunter kill is not guaranteed...
 	else if (placeIsSea(currLoc) && 
 			 (numHunterSameSea > 1 
 			 || (numHunterSameSea == 1 
 			 	&& guaranteeHunterKillIfSameLoc(round, healthHunterSameSea, locFallingOffTrail, furthestNonSeaLoc, numTrapsAtNonSeaLoc))))
-		registerBestPlay(dracLocToMoveAbbrev(furthestLoc), "...");
+		registerBestPlay(dracLocToMoveAbbrev(dv, furthestLoc), "...");
 	// ... then to placing the immature vampire...
-	else if (round % 13 == 0) registerBestPlay(dracLocToMoveAbbrev(furthestNonSeaLoc), "...");
+	else if (round % 13 == 0) registerBestPlay(dracLocToMoveAbbrev(dv, furthestNonSeaLoc), "...");
 	// ... then to making his way to Castle Dracula, if health is low...
 	// else if (health < LOW_HEALTH) find shortest way to castle dracula - could be either by self-restriction or travelling
 	// ... then to separating himself from the hunters, avoiding the sea...
 		// If heading to sea means that one less encounter can be made with a hunter before death, then
 		// head to the best city location instead.	
 	else if ((DvGetHealth(dv, PLAYER_DRACULA) - LIFE_LOSS_SEA) % LIFE_LOSS_HUNTER_ENCOUNTER == 0)
-		registerBestPlay(dracLocToMoveAbbrev(furthestNonSeaLoc), "...");
+		registerBestPlay(dracLocToMoveAbbrev(dv, furthestNonSeaLoc), "...");
 	else 
-		registerBestPlay(dracLocToMoveAbbrev(furthestLoc), "...");
+		registerBestPlay(dracLocToMoveAbbrev(dv, furthestLoc), "...");
 }
 
 void handleRoundZero(DraculaView dv) {
@@ -170,7 +169,7 @@ void handleRoundZero(DraculaView dv) {
 		totalScatter = 0;
 	}
 	
-	if (bestLoc != MIN_REAL_PLACE) registerBestPlay(dracLocToMoveAbbrev(bestLoc), "Mwahahahaha");
+	if (bestLoc != MIN_REAL_PLACE) registerBestPlay(dracLocToMoveAbbrev(dv, bestLoc), "Mwahahahaha");
 }
 
 int distFromHunters(DraculaView dv, PlaceId loc) {
@@ -234,11 +233,6 @@ int shortestPathFrom(DraculaView dv, Player hunter, PlaceId dest)
 	}
 
 	return pathLength;
-}
-
-// TODO: don't need this bc starter files changed in hunt spec
-char *placeIdToAbbrevNonConst(PlaceId pid) {
-	return strdup(placeIdToAbbrev(pid));
 }
 
 // Returns true if the location exists in the array, and false if
